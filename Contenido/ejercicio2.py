253cecc46913a9cfce9e0ffa0b427f206bae40e0
MarcoSaldivia <marcoantonio.saldivia@alumnos.ulagos.cl>
2025-09-03 11:57:33 -0400

    Actualizando ejercicio 2 hecho en Clases (No terminado)

--- Contenido/ejercicio2.py
+++ Contenido/ejercicio2.py
@@ -6,4 +6,25 @@ class Coche:
         self.gasolina = float(gasolina)
         self.kilometros_recorridos = 0
 
-    def 
+    def conducir(self, km):
+        litros_necesarios = km / 10
+        if litros_necesarios <= self.gasolina:
+            self.kilometros_recorridos += km
+            self.gasolina -= litros_necesarios
+            print(f"Conduciste {km} km. Gasolina restante: {self.gasolina} L")
+
+        else:
+            km_posibles = self.gasolina * 10
+            self.kilometros_recorridos += km_posibles
+            self.gasolina = 0
+            print(f"Solo pudiste conducir {km_posibles} km. Te quedaste sin gasolina.")
+
+    def cargar_gasolina(self, litros):
+        self.gasolina += float(litros)
+        print(f"Cargaste {litros} L. Gasolina actual: {self.gasolina} L")
+
+
+
+# FALTA CREAR OBJETO
+coche1 = Coche()
+coche2 = Coche()
